3ed0629d59152f1ebf91f149dd8e6f547791641d
Ankit yadav <rohityadav9759728519@gmail.com>
2026-06-24 11:18:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,19 @@
-const select = (query) => document.querySelector(query)
-let input =  select(".form .input input")
-let won =  select(".won")
-let form =  select(".form")
-let tries = select(".tries")
-let errorMsg = select(".input-error-check")
-let hint = select('.hint p')
-let answer = Math.ceil(Math.random() * 99)
-let trial = 0
+function checkNumber() {
 
-select("#check").addEventListener('click', () => {
-    if (!Number(input.value)) errorMsg.classList.add('show')
-    else {
-        errorMsg.classList.remove('show')
-       
-        trial++
-        let inputNumber = Number(input.value) // this is the user's entered number
-        if (inputNumber > answer) {
-            hint.innerHTML = "you need to guess a <span>smaller</span> number"
-        }
-        else if (inputNumber < answer) {
-            hint.innerHTML = "you need to guess a <span>greater</span> number"
-        }
-        if (inputNumber === answer) {
-            won.classList.add("show")
-            form.classList.add("hide")
-            tries.innerText = trial
-        }
-        input.value = ''
+    let number = document.getElementById("num").value;
+
+    if(number === ""){
+        document.getElementById("result").innerHTML = "Please Enter a Number";
+        return;
     }
-})
 
-select("#replay").addEventListener('click', () => {
-    answer = Math.ceil(Math.random() * 99)
-    trial = 0
-    won.classList.remove("show")
-    form.classList.remove("hide")
-})
+    if(number % 2 == 0){
+        document.getElementById("result").innerHTML =
+        number + " is an Even Number";
+    }
+    else{
+        document.getElementById("result").innerHTML =
+        number + " is an Odd Number";
+    }
+
+}
