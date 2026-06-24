039b4d09d767530335c40a299ce6bcc37788e1f7
Ankit yadav <rohityadav9759728519@gmail.com>
2026-06-24 10:16:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-// Cart Counter functionality
-let cartCount = 0;
-const cartCountElement = document.getElementById('cart-count');
-const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
+const select = (query) => document.querySelector(query)
+let input =  select(".form .input input")
+let won =  select(".won")
+let form =  select(".form")
+let tries = select(".tries")
+let errorMsg = select(".input-error-check")
+let hint = select('.hint p')
+let answer = Math.ceil(Math.random() * 99)
+let trial = 0
 
-// Har ek button par event listener lagana
-addToCartButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        cartCount++;
-        cartCountElement.textContent = cartCount;
-        
-        // Ek simple alert validation ke liye (Optional)
-        alert('Item added to your Amazon Cart!');
-    });
-});
+select("#check").addEventListener('click', () => {
+    if (!Number(input.value)) errorMsg.classList.add('show')
+    else {
+        errorMsg.classList.remove('show')
+       
+        trial++
+        let inputNumber = Number(input.value) // this is the user's entered number
+        if (inputNumber > answer) {
+            hint.innerHTML = "you need to guess a <span>smaller</span> number"
+        }
+        else if (inputNumber < answer) {
+            hint.innerHTML = "you need to guess a <span>greater</span> number"
+        }
+        if (inputNumber === answer) {
+            won.classList.add("show")
+            form.classList.add("hide")
+            tries.innerText = trial
+        }
+        input.value = ''
+    }
+})
 
-// Back to Top button functionality
-const backToTop = document.querySelector('.foot-panel1');
-backToTop.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
+select("#replay").addEventListener('click', () => {
+    answer = Math.ceil(Math.random() * 99)
+    trial = 0
+    won.classList.remove("show")
+    form.classList.remove("hide")
+})
